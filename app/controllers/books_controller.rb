f4e90347270c5df9155d47aaacffa1e79384a9a2
laemdfk <kappa021618@icloud.com>
2022-07-11 13:03:28 +0000

    復習開始

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    end
+      
+  end
+  
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.save
+      redirect_to book_path(params[:id])
+    end
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      redirect_to books_path
+    end
+  end
+  
+  
+ private
+  
+  def book_params
+    params.require(book).permit(:title, :body)
+  end
+  
+end
