bbfedf5e28bc1dc38a732351e6d5598d6a68c839
laemdfk <kappa021618@icloud.com>
2022-07-13 14:38:04 +0000

    [Add]投稿詳細画面の作成、投稿フォーム変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,23 @@
 class BooksController < ApplicationController
-  def create
-    @book = Book.new(book_params)
-    if @book.save
-      redirect_to books_path
-    end
-      
-  end
-  
+ 
+# プログラムは上から順にしか読み込めない
   def index
     @books = Book.all
+    @book = Book.new
   end
-
+  
+  def create
+    @book = Book.new(book_params)
+     if @book.save
+      flash[:notice] ="Book was successfully created."
+      redirect_to book_path(@book.id)
+      
+     else
+       @books = Book.all
+       render  'index'
+     end
+  end
+  
   def show
     @book = Book.find(params[:id])
   end
@@ -18,26 +25,27 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
-    @book = Book.find(params[:id])
-    if @book.save
-      redirect_to book_path(params[:id])
-    end
+     @book = Book.find(params[:id])
+     if @book.update(book_params)
+      flash[:notice] = "Book was successfully update."
+     redirect_to book_path#(book.id)
+     else
+     render 'edit'
+     end
   end
   
   def destroy
     @book = Book.find(params[:id])
-    if @book.destroy
-      redirect_to books_path
-    end
-  end
-  
+    @book.destroy
+    flash[:notice]="Book was successfully destroyed."
+    redirect_to books_path
+  end 
   
  private
-  
+# require後の:忘れない
   def book_params
-    params.require(book).permit(:title, :body)
+  params.require(:book).permit(:title, :body)
   end
-  
 end
